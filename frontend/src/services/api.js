1b1d680cc05449f62e6f81d2dde49672e75eaf40
Khanqueror456 <unrealengine456@gmail.com>
2026-09-11 19:40:32 +0530

    Update env.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
+    baseURL: process.env.FRONTEND_URL || 'http://localhost:3000',
     withCredentials : true,
     timeout: 3000000
 })
+
+// console.log(process.env.REACT_APP_API_URL);
 
 export default api;
