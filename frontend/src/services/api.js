a3b26ed8983edcf4f904beb580dbeacaf509dc77
Khanqueror456 <unrealengine456@gmail.com>
2026-09-11 20:38:35 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // ✅ MUST start with REACT_APP_ and point to the RENDER backend
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000",
+    baseURL: process.env.REACT_APP_API_URL,
     withCredentials: true,
     timeout: 3000000
 })
