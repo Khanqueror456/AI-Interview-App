282d382717d64a519cdde413a07d725c97d25ac1
Khanqueror456 <unrealengine456@gmail.com>
2026-09-11 18:17:22 +0530

    Update db .js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://unrealengine456_db_user:Mdsaad@mongodb456@cluster0.kuwrdhq.mongodb.net/?appName=Cluster0");
+        const conn = await mongoose.connect("mongodb+srv://unrealengine456_db_user:<Mdsaad@mongodb456>@cluster0.kuwrdhq.mongodb.net/?appName=Cluster0");
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
