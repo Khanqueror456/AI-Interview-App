92d3b9491688d72d04a7a4c94a89a1cfd938133e
Khanqueror456 <unrealengine456@gmail.com>
2026-09-14 17:32:24 +0530

    #

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -37,7 +37,8 @@ export const signup = asyncHandler(async (req, res) => {
 
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        // secure: process.env.NODE_ENV === "production",
+        secure : true,
         sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
@@ -79,7 +80,8 @@ export const login = asyncHandler(async (req, res) => {
 
     res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        // secure: process.env.NODE_ENV === "production",
+        secure : true,
         sameSite: "none",
         maxAge: 1 * 24 * 60 * 60 * 1000
     })
